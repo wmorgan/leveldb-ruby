9a230873f2364e0dc96f76d230a1d399ebab484f
Johannes Holzfuß <DataWraith@web.de>
2011-10-16 11:51:24 +0200

    Call #to_str on the argument given to DB#{new, create, load}
    This matches the behavior of `File.open`. In particular, it allows us
    to pass Pathname objects (from the pathname standard library) to the
    DB functions.

--- lib/leveldb.rb
+++ lib/leveldb.rb
@@ -8,19 +8,19 @@ class DB
     ## Loads or creates a LevelDB database as necessary, stored on disk at
     ## +pathname+.
     def new pathname
-      make pathname, true, false
+      make pathname.to_str, true, false
     end
 
     ## Creates a new LevelDB database stored on disk at +pathname+. Throws an
     ## exception if the database already exists.
     def create pathname
-      make pathname, true, true
+      make pathname.to_str, true, true
     end
 
     ## Loads a LevelDB database stored on disk at +pathname+. Throws an
     ## exception unless the database already exists.
     def load pathname
-      make pathname, false, false
+      make pathname.to_str, false, false
     end
   end
 
