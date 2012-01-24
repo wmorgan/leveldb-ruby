d882439cc5f5cf1d7241b5363df7bf06664b7938
yukio.goto <yukio.goto@mail.rakuten.com>
2012-01-24 09:19:30 +0900

    change static methods to non name namespace methods

--- ext/leveldb/leveldb.cc
+++ ext/leveldb/leveldb.cc
@@ -27,298 +27,297 @@ static leveldb::ReadOptions uncached_read_options;
   }  \
 } while(0)
 
-bool hash_val_test(VALUE h, VALUE key) {
-  VALUE v = rb_hash_aref(h, key);
-  return RTEST(v);
-}
-
-typedef struct bound_db {
-  leveldb::DB* db;
-} bound_db;
-
-static void db_free(bound_db* db) {
-  if(db->db != NULL) {
-    delete db->db;
-    db->db = NULL;
-  }
-  delete db;
-}
-
-static VALUE db_make(VALUE klass, VALUE params) {
-  Check_Type(params, T_HASH);
-
-  VALUE path = rb_hash_aref(params, ID2SYM(rb_intern("path")));
-  Check_Type(path, T_STRING);
-
-  bound_db* db = new bound_db;
-  std::string pathname = std::string((char*)RSTRING_PTR(path));
-
-  leveldb::Options options;
-  if(hash_val_test(params, ID2SYM(rb_intern("create_if_missing")))) {
-    options.create_if_missing = true;
+namespace {
+  bool hash_val_test(VALUE h, VALUE key) {
+    VALUE v = rb_hash_aref(h, key);
+    return RTEST(v);
   }
 
-  if(hash_val_test(params, ID2SYM(rb_intern("error_if_exists")))) {
-    options.error_if_exists = true;
-  }
-  leveldb::Status status = leveldb::DB::Open(options, pathname, &db->db);
-  RAISE_ON_ERROR(status);
+  typedef struct bound_db {
+    leveldb::DB* db;
+  } bound_db;
 
-  VALUE o_db = Data_Wrap_Struct(klass, NULL, db_free, db);
-  VALUE argv[1] = { path };
-  rb_obj_call_init(o_db, 1, argv);
-
-  return o_db;
-}
-
-static VALUE db_close(VALUE self) {
-  bound_db* db;
-  Data_Get_Struct(self, bound_db, db);
-
-  if(db->db != NULL) {
-    delete db->db;
-    db->db = NULL;
-  }
-  return Qtrue;
-}
-
-static leveldb::ReadOptions parse_read_options(VALUE options) {
-  leveldb::ReadOptions readOptions;
-
-  if(!NIL_P(options)) {
-    Check_Type(options, T_HASH);
-    if(rb_hash_aref(options, k_fill) == Qfalse)
-      readOptions.fill_cache = false;
-    if(rb_hash_aref(options, k_verify) == Qtrue)
-      readOptions.verify_checksums = true;
-  }
-
-  return readOptions;
-}
-
-static leveldb::WriteOptions parse_write_options(VALUE options) {
-  leveldb::WriteOptions writeOptions;
-
-  if(!NIL_P(options)) {
-    Check_Type(options, T_HASH);
-    if(rb_hash_aref(options, k_sync) == Qtrue)
-      writeOptions.sync = true;
-  }
-
-  return writeOptions;
-}
-
-#define RUBY_STRING_TO_SLICE(x) leveldb::Slice(RSTRING_PTR(x), RSTRING_LEN(x))
-#define SLICE_TO_RUBY_STRING(x) rb_str_new(x.data(), x.size())
-#define STRING_TO_RUBY_STRING(x) rb_str_new(x.data(), x.size())
-static VALUE db_get(int argc, VALUE* argv, VALUE self) {
-  VALUE v_key, v_options;
-  rb_scan_args(argc, argv, "11", &v_key, &v_options);
-  Check_Type(v_key, T_STRING);
-  leveldb::ReadOptions readOptions = parse_read_options(v_options);
-
-  bound_db* db;
-  Data_Get_Struct(self, bound_db, db);
-
-  leveldb::Slice key = RUBY_STRING_TO_SLICE(v_key);
-  std::string value;
-  leveldb::Status status = db->db->Get(readOptions, key, &value);
-  if(status.IsNotFound()) return Qnil;
-
-  RAISE_ON_ERROR(status);
-  return STRING_TO_RUBY_STRING(value);
-}
-
-static VALUE db_delete(int argc, VALUE* argv, VALUE self) {
-  VALUE v_key, v_options;
-  rb_scan_args(argc, argv, "11", &v_key, &v_options);
-  Check_Type(v_key, T_STRING);
-  leveldb::WriteOptions writeOptions = parse_write_options(v_options);
-
-  bound_db* db;
-  Data_Get_Struct(self, bound_db, db);
-
-  leveldb::Slice key = RUBY_STRING_TO_SLICE(v_key);
-  std::string value;
-  leveldb::Status status = db->db->Get(uncached_read_options, key, &value);
-
-  if(status.IsNotFound()) return Qnil;
-
-  status = db->db->Delete(writeOptions, key);
-  RAISE_ON_ERROR(status);
-
-  return STRING_TO_RUBY_STRING(value);
-}
-
-static VALUE db_exists(VALUE self, VALUE v_key) {
-  Check_Type(v_key, T_STRING);
-
-  bound_db* db;
-  Data_Get_Struct(self, bound_db, db);
-
-  leveldb::Slice key = RUBY_STRING_TO_SLICE(v_key);
-  std::string value;
-  leveldb::Status status = db->db->Get(leveldb::ReadOptions(), key, &value);
-
-  if(status.IsNotFound()) return Qfalse;
-  return Qtrue;
-}
-
-static VALUE db_put(int argc, VALUE* argv, VALUE self) {
-  VALUE v_key, v_value, v_options;
-
-  rb_scan_args(argc, argv, "21", &v_key, &v_value, &v_options);
-  Check_Type(v_key, T_STRING);
-  Check_Type(v_value, T_STRING);
-  leveldb::WriteOptions writeOptions = parse_write_options(v_options);
-
-  bound_db* db;
-  Data_Get_Struct(self, bound_db, db);
-
-  leveldb::Slice key = RUBY_STRING_TO_SLICE(v_key);
-  leveldb::Slice value = RUBY_STRING_TO_SLICE(v_value);
-  leveldb::Status status = db->db->Put(writeOptions, key, value);
-
-  RAISE_ON_ERROR(status);
-
-  return v_value;
-}
-
-static VALUE db_size(VALUE self) {
-  long count = 0;
-
-  bound_db* db;
-  Data_Get_Struct(self, bound_db, db);
-  leveldb::Iterator* it = db->db->NewIterator(uncached_read_options);
-
-  // apparently this is how we have to do it. slow and painful!
-  for (it->SeekToFirst(); it->Valid(); it->Next()) count++;
-  RAISE_ON_ERROR(it->status());
-  delete it;
-  return INT2NUM(count);
-}
-
-static VALUE db_iterate(VALUE self, VALUE key_from, VALUE key_to, bool reversed) {
-  bound_db* db;
-  Data_Get_Struct(self, bound_db, db);
-  leveldb::Iterator* it = db->db->NewIterator(uncached_read_options);
-
-  if(RTEST(key_from)) {
-    it->Seek(RUBY_STRING_TO_SLICE(rb_funcall(key_from, to_s, 0)));
-  } else {
-    if(reversed) {
-      it->SeekToLast();
-    } else {
-      it->SeekToFirst();
+  void db_free(bound_db* db) {
+    if(db->db != NULL) {
+      delete db->db;
+      db->db = NULL;
     }
+    delete db;
   }
 
-  bool passed_limit = false;
-  bool check_limit = RTEST(key_to);
-  std::string key_to_str;
+  VALUE db_make(VALUE klass, VALUE params) {
+    Check_Type(params, T_HASH);
 
-  if(check_limit)
-    key_to_str = RUBY_STRING_TO_SLICE(rb_funcall(key_to, to_s, 0)).ToString();
+    VALUE path = rb_hash_aref(params, ID2SYM(rb_intern("path")));
+    Check_Type(path, T_STRING);
 
-  while(!passed_limit && it->Valid()) {
-    leveldb::Slice key_sl = it->key();
+    bound_db* db = new bound_db;
+    std::string pathname = std::string((char*)RSTRING_PTR(path));
 
-    if(check_limit &&
-        (reversed ?
+    leveldb::Options options;
+    if(hash_val_test(params, ID2SYM(rb_intern("create_if_missing")))) {
+      options.create_if_missing = true;
+    }
+
+    if(hash_val_test(params, ID2SYM(rb_intern("error_if_exists")))) {
+      options.error_if_exists = true;
+    }
+    leveldb::Status status = leveldb::DB::Open(options, pathname, &db->db);
+    RAISE_ON_ERROR(status);
+
+    VALUE o_db = Data_Wrap_Struct(klass, NULL, db_free, db);
+    VALUE argv[1] = { path };
+    rb_obj_call_init(o_db, 1, argv);
+
+    return o_db;
+  }
+
+  VALUE db_close(VALUE self) {
+    bound_db* db;
+    Data_Get_Struct(self, bound_db, db);
+
+    if(db->db != NULL) {
+      delete db->db;
+      db->db = NULL;
+    }
+    return Qtrue;
+  }
+
+  leveldb::ReadOptions parse_read_options(VALUE options) {
+    leveldb::ReadOptions readOptions;
+
+    if(!NIL_P(options)) {
+      Check_Type(options, T_HASH);
+      if(rb_hash_aref(options, k_fill) == Qfalse)
+        readOptions.fill_cache = false;
+      if(rb_hash_aref(options, k_verify) == Qtrue)
+        readOptions.verify_checksums = true;
+    }
+
+    return readOptions;
+  }
+
+  leveldb::WriteOptions parse_write_options(VALUE options) {
+    leveldb::WriteOptions writeOptions;
+
+    if(!NIL_P(options)) {
+      Check_Type(options, T_HASH);
+      if(rb_hash_aref(options, k_sync) == Qtrue)
+        writeOptions.sync = true;
+    }
+
+    return writeOptions;
+  }
+
+  VALUE db_get(int argc, VALUE* argv, VALUE self) {
+    VALUE v_key, v_options;
+    rb_scan_args(argc, argv, "11", &v_key, &v_options);
+    Check_Type(v_key, T_STRING);
+    leveldb::ReadOptions readOptions = parse_read_options(v_options);
+
+    bound_db* db;
+    Data_Get_Struct(self, bound_db, db);
+
+    leveldb::Slice key = RUBY_STRING_TO_SLICE(v_key);
+    std::string value;
+    leveldb::Status status = db->db->Get(readOptions, key, &value);
+    if(status.IsNotFound()) return Qnil;
+
+    RAISE_ON_ERROR(status);
+    return STRING_TO_RUBY_STRING(value);
+  }
+
+  VALUE db_delete(int argc, VALUE* argv, VALUE self) {
+    VALUE v_key, v_options;
+    rb_scan_args(argc, argv, "11", &v_key, &v_options);
+    Check_Type(v_key, T_STRING);
+    leveldb::WriteOptions writeOptions = parse_write_options(v_options);
+
+    bound_db* db;
+    Data_Get_Struct(self, bound_db, db);
+
+    leveldb::Slice key = RUBY_STRING_TO_SLICE(v_key);
+    std::string value;
+    leveldb::Status status = db->db->Get(uncached_read_options, key, &value);
+
+    if(status.IsNotFound()) return Qnil;
+
+    status = db->db->Delete(writeOptions, key);
+    RAISE_ON_ERROR(status);
+
+    return STRING_TO_RUBY_STRING(value);
+  }
+
+  VALUE db_exists(VALUE self, VALUE v_key) {
+    Check_Type(v_key, T_STRING);
+
+    bound_db* db;
+    Data_Get_Struct(self, bound_db, db);
+
+    leveldb::Slice key = RUBY_STRING_TO_SLICE(v_key);
+    std::string value;
+    leveldb::Status status = db->db->Get(leveldb::ReadOptions(), key, &value);
+
+    if(status.IsNotFound()) return Qfalse;
+    return Qtrue;
+  }
+
+  VALUE db_put(int argc, VALUE* argv, VALUE self) {
+    VALUE v_key, v_value, v_options;
+
+    rb_scan_args(argc, argv, "21", &v_key, &v_value, &v_options);
+    Check_Type(v_key, T_STRING);
+    Check_Type(v_value, T_STRING);
+    leveldb::WriteOptions writeOptions = parse_write_options(v_options);
+
+    bound_db* db;
+    Data_Get_Struct(self, bound_db, db);
+
+    leveldb::Slice key = RUBY_STRING_TO_SLICE(v_key);
+    leveldb::Slice value = RUBY_STRING_TO_SLICE(v_value);
+    leveldb::Status status = db->db->Put(writeOptions, key, value);
+
+    RAISE_ON_ERROR(status);
+
+    return v_value;
+  }
+
+  VALUE db_size(VALUE self) {
+    long count = 0;
+
+    bound_db* db;
+    Data_Get_Struct(self, bound_db, db);
+    leveldb::Iterator* it = db->db->NewIterator(uncached_read_options);
+
+    // apparently this is how we have to do it. slow and painful!
+    for (it->SeekToFirst(); it->Valid(); it->Next()) count++;
+    RAISE_ON_ERROR(it->status());
+    delete it;
+    return INT2NUM(count);
+  }
+
+  VALUE db_iterate(VALUE self, VALUE key_from, VALUE key_to, bool reversed) {
+    bound_db* db;
+    Data_Get_Struct(self, bound_db, db);
+    leveldb::Iterator* it = db->db->NewIterator(uncached_read_options);
+
+    if(RTEST(key_from)) {
+      it->Seek(RUBY_STRING_TO_SLICE(rb_funcall(key_from, to_s, 0)));
+    } else {
+      if(reversed) {
+        it->SeekToLast();
+      } else {
+        it->SeekToFirst();
+      }
+    }
+
+    bool passed_limit = false;
+    bool check_limit = RTEST(key_to);
+    std::string key_to_str;
+
+    if(check_limit)
+      key_to_str = RUBY_STRING_TO_SLICE(rb_funcall(key_to, to_s, 0)).ToString();
+
+    while(!passed_limit && it->Valid()) {
+      leveldb::Slice key_sl = it->key();
+
+      if(check_limit &&
+         (reversed ?
           (key_sl.ToString() < key_to_str) :
           (key_sl.ToString() > key_to_str))) {
-      passed_limit = true;
-    } else {
-      VALUE key = SLICE_TO_RUBY_STRING(key_sl);
-      VALUE value = SLICE_TO_RUBY_STRING(it->value());
-      VALUE ary = rb_ary_new2(2);
-      rb_ary_push(ary, key);
-      rb_ary_push(ary, value);
-      rb_yield(ary);
-      reversed ? it->Prev() : it->Next();
+        passed_limit = true;
+      } else {
+        VALUE key = SLICE_TO_RUBY_STRING(key_sl);
+        VALUE value = SLICE_TO_RUBY_STRING(it->value());
+        VALUE ary = rb_ary_new2(2);
+        rb_ary_push(ary, key);
+        rb_ary_push(ary, value);
+        rb_yield(ary);
+        reversed ? it->Prev() : it->Next();
+      }
     }
+
+    RAISE_ON_ERROR(it->status());
+    delete it;
+
+    return self;
   }
 
-  RAISE_ON_ERROR(it->status());
-  delete it;
+  VALUE db_each(int argc, VALUE* argv, VALUE self) {
+    VALUE key_from, key_to;
+    rb_scan_args(argc, argv, "02", &key_from, &key_to);
 
-  return self;
-}
+    return db_iterate(self, key_from, key_to, false);
+  }
 
-static VALUE db_each(int argc, VALUE* argv, VALUE self) {
-  VALUE key_from, key_to;
-  rb_scan_args(argc, argv, "02", &key_from, &key_to);
+  VALUE db_reverse_each(int argc, VALUE* argv, VALUE self) {
+    VALUE key_from, key_to;
+    rb_scan_args(argc, argv, "02", &key_from, &key_to);
 
-  return db_iterate(self, key_from, key_to, false);
-}
+    return db_iterate(self, key_from, key_to, true);
+  }
 
-static VALUE db_reverse_each(int argc, VALUE* argv, VALUE self) {
-  VALUE key_from, key_to;
-  rb_scan_args(argc, argv, "02", &key_from, &key_to);
+  VALUE db_init(VALUE self, VALUE v_pathname) {
+    rb_iv_set(self, "@pathname", v_pathname);
+    return self;
+  }
 
-  return db_iterate(self, key_from, key_to, true);
-}
+  typedef struct bound_batch {
+    leveldb::WriteBatch batch;
+  } bound_batch;
 
-static VALUE db_init(VALUE self, VALUE v_pathname) {
-  rb_iv_set(self, "@pathname", v_pathname);
-  return self;
-}
+  void batch_free(bound_batch* batch) {
+    delete batch;
+  }
 
-typedef struct bound_batch {
-  leveldb::WriteBatch batch;
-} bound_batch;
+  VALUE batch_make(VALUE klass) {
+    bound_batch* batch = new bound_batch;
+    batch->batch = leveldb::WriteBatch();
 
-static void batch_free(bound_batch* batch) {
-  delete batch;
-}
+    VALUE o_batch = Data_Wrap_Struct(klass, NULL, batch_free, batch);
+    VALUE argv[0];
+    rb_obj_call_init(o_batch, 0, argv);
 
-static VALUE batch_make(VALUE klass) {
-  bound_batch* batch = new bound_batch;
-  batch->batch = leveldb::WriteBatch();
+    return o_batch;
+  }
 
-  VALUE o_batch = Data_Wrap_Struct(klass, NULL, batch_free, batch);
-  VALUE argv[0];
-  rb_obj_call_init(o_batch, 0, argv);
+  VALUE batch_put(VALUE self, VALUE v_key, VALUE v_value) {
+    Check_Type(v_key, T_STRING);
+    Check_Type(v_value, T_STRING);
 
-  return o_batch;
-}
+    bound_batch* batch;
+    Data_Get_Struct(self, bound_batch, batch);
+    batch->batch.Put(RUBY_STRING_TO_SLICE(v_key), RUBY_STRING_TO_SLICE(v_value));
 
-static VALUE batch_put(VALUE self, VALUE v_key, VALUE v_value) {
-  Check_Type(v_key, T_STRING);
-  Check_Type(v_value, T_STRING);
+    return v_value;
+  }
 
-  bound_batch* batch;
-  Data_Get_Struct(self, bound_batch, batch);
-  batch->batch.Put(RUBY_STRING_TO_SLICE(v_key), RUBY_STRING_TO_SLICE(v_value));
+  VALUE batch_delete(VALUE self, VALUE v_key) {
+    Check_Type(v_key, T_STRING);
+    bound_batch* batch;
+    Data_Get_Struct(self, bound_batch, batch);
+    batch->batch.Delete(RUBY_STRING_TO_SLICE(v_key));
+    return Qtrue;
+  }
 
-  return v_value;
-}
+  VALUE db_batch(int argc, VALUE* argv, VALUE self) {
+    VALUE o_batch = batch_make(c_batch);
 
-static VALUE batch_delete(VALUE self, VALUE v_key) {
-  Check_Type(v_key, T_STRING);
-  bound_batch* batch;
-  Data_Get_Struct(self, bound_batch, batch);
-  batch->batch.Delete(RUBY_STRING_TO_SLICE(v_key));
-  return Qtrue;
-}
+    rb_yield(o_batch);
 
-static VALUE db_batch(int argc, VALUE* argv, VALUE self) {
-  VALUE o_batch = batch_make(c_batch);
+    bound_batch* batch;
+    bound_db* db;
+    Data_Get_Struct(o_batch, bound_batch, batch);
+    Data_Get_Struct(self, bound_db, db);
 
-  rb_yield(o_batch);
+    VALUE v_options;
+    rb_scan_args(argc, argv, "01", &v_options);
+    leveldb::WriteOptions writeOptions = parse_write_options(v_options);
 
-  bound_batch* batch;
-  bound_db* db;
-  Data_Get_Struct(o_batch, bound_batch, batch);
-  Data_Get_Struct(self, bound_db, db);
-
-  VALUE v_options;
-  rb_scan_args(argc, argv, "01", &v_options);
-  leveldb::WriteOptions writeOptions = parse_write_options(v_options);
-
-  leveldb::Status status = db->db->Write(writeOptions, &batch->batch);
-  RAISE_ON_ERROR(status);
-  return Qtrue;
+    leveldb::Status status = db->db->Write(writeOptions, &batch->batch);
+    RAISE_ON_ERROR(status);
+    return Qtrue;
+  }
 }
 
 extern "C" {
