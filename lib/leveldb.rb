f3bbc718545a5dff52ef451a7766907d4e690b02
byplayer <byplayer100@gmail.com>
2012-02-11 11:48:24 +0900

    fix document and change make method to open

--- lib/leveldb.rb
+++ lib/leveldb.rb
@@ -17,7 +17,7 @@ module LevelDB
         end
         options[:create_if_missing] ||= true
         options[:error_if_exists] ||= false
-        make(options)
+        open(options)
       end
 
       ## Creates a new LevelDB database stored on disk at +pathname+. Throws an
@@ -32,7 +32,7 @@ module LevelDB
         end
         options[:create_if_missing] ||= true
         options[:error_if_exists] ||= true
-        make(options)
+        open(options)
       end
 
       ## Loads a LevelDB database stored on disk at +pathname+. Throws an
@@ -47,7 +47,7 @@ module LevelDB
         end
         options[:create_if_missing] ||= false
         options[:error_if_exists] ||= false
-        make(options)
+        open(options)
       end
 
       private
@@ -59,36 +59,6 @@ module LevelDB
     end
 
     attr_reader :options
-
-    # get data from db
-    # key:: key you want to get
-    # options[:fill_cache] Should the data read for this iteration be cached in memory?
-    #                      Callers may wish to set this field to false for bulk scans.
-    #                      true or false
-    #                      Default: true
-    # options[:verify_checksums] If true, all data read from underlying storage will be
-    #                            verified against corresponding checksums.
-    #                            Default: false
-    # return:: value of stored db
-
-    # put
-    # options[:sync]:: If true, the write will be flushed from the operating system
-    #                  buffer cache (by calling WritableFile::Sync()) before the write
-    #                  is considered complete.  If this flag is true, writes will be
-    #                  slower.
-    #
-    #                  If this flag is false, and the machine crashes, some recent
-    #                  writes may be lost.  Note that if it is just the process that
-    #                  crashes (i.e., the machine does not reboot), no writes will be
-    #                  lost even if sync==false.
-    #
-    #                  In other words, a DB write with sync==false has similar
-    #                  crash semantics as the "write()" system call.  A DB write
-    #                  with sync==true has similar crash semantics to a "write()"
-    #                  system call followed by "fsync()".
-    #
-    #                  Default: false
-    # returns:: stored value
 
     alias :includes? :exists?
     alias :contains? :exists?
