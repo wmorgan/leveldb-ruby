9107a4030cf5f869715c3a7dcdc10f427e44f78f
Nick White <nwhite@palantir.com>
2014-08-26 10:16:29 +0100

    Use pkg-config to find system leveldb

--- ext/leveldb/extconf.rb
+++ ext/leveldb/extconf.rb
@@ -2,13 +2,9 @@ require 'mkmf'
 require 'fileutils'
 require "./platform.rb"
 
-Dir.chdir "../../leveldb"
-system "OPT=\"-O2 -DNDEBUG -fPIC\" make libleveldb.a" or abort
-Dir.chdir "../ext/leveldb"
-
 set_platform_specific_variables!
 
-$CFLAGS << " -I../../leveldb/include"
-$LIBS << " -L../../leveldb -lleveldb"
+$INCFLAGS << ' ' << `pkg-config --cflags-only-I leveldb`.strip << ' '
+$LIBS << ' ' << `pkg-config --libs leveldb`.strip << ' '
 
 create_makefile "leveldb/leveldb"
